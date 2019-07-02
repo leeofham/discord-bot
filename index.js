6f404054925664b76a148af17dc5d9dcebd88f1c
Liam Roberts <liamroberts300@gmail.com>
2019-07-02 10:18:06 +0100

    got bot setup on server and replies to hello message inputed by user

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const Discord = require('discord.js')
-const bot = new Discord.Client()
-
 const dotenv = require('dotenv')
 dotenv.config()
 
-bot.login('process.env.DISCORD_TOKEN')
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+bot.on('message', (message) =>{
+  if(message.content === 'hello'){
+    // message.reply('hello')
+    message.channel.sendMessage('hello')
+  }
+})
+
+bot.login(process.env.DISCORD_TOKEN)
